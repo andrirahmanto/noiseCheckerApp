8e961ecfff4e77073e91fcc3f45bc6346b1d0f0e
Andri Rahmanto <rahmanto660@gmail.com>
2021-12-16 15:54:55 +0700

    fix: fix custom text

--- lib/utils/custom_text.util.dart
+++ lib/utils/custom_text.util.dart
@@ -1,34 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:noisechecker/utils/styles.util.dart';
-import 'package:styled_text/styled_text.dart';
 
-class CustomText extends StatelessWidget {
+class PrimaryText extends StatelessWidget {
   final String text;
-  final double fontSize;
-  final FontWeight? fontWeight;
   final Color? color;
-  final int? maxLines;
-  final TextAlign? textAlign;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final TextOverflow? overflow;
 
-  CustomText(
-      {required this.text,
-      required this.fontSize,
-      this.fontWeight = FontWeight.w500,
-      this.color = ColorPalettes.black,
-      this.maxLines = 1,
-      this.textAlign = TextAlign.left});
+  const PrimaryText({
+    required this.text,
+    required this.fontSize,
+    this.color: ColorPalettes.white,
+    this.fontWeight: FontWeight.w700,
+    this.overflow: TextOverflow.visible,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return StyledText(
-      text: text,
-      maxLines: maxLines,
-      textAlign: textAlign!,
+    return Text(
+      text,
+      overflow: overflow,
       style: TextStyle(
-        fontSize: fontSize,
-        fontWeight: fontWeight,
         color: color,
+        fontSize: fontSize,
+        fontFamily: 'Quicksand',
+        fontWeight: fontWeight,
       ),
     );
   }
 }
+// class CustomText extends StatelessWidget {
+//   final String text;
+//   final double fontSize;
+//   final FontWeight? fontWeight;
+//   final Color? color;
+//   final int? maxLines;
+//   final TextAlign? textAlign;
+
+//   CustomText(
+//       {required this.text,
+//       required this.fontSize,
+//       this.fontWeight = FontWeight.w500,
+//       this.color = ColorPalettes.black,
+//       this.maxLines = 1,
+//       this.textAlign = TextAlign.left});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return StyledText(
+//       text: text,
+//       maxLines: maxLines,
+//       textAlign: textAlign!,
+//       style: TextStyle(
+//         fontSize: fontSize,
+//         fontWeight: fontWeight,
+//         color: color,
+//       ),
+//     );
+//   }
+// }
