4487f8365ffffe05fedadaafd478206752868a36
Andri Rahmanto <rahmanto660@gmail.com>
2021-12-16 21:53:16 +0700

    feat: add card to webview

--- lib/screens/main.screen.dart
+++ lib/screens/main.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noisechecker/screens/webview.screen.dart';
 import 'package:noisechecker/utils/custom_text.util.dart';
 import 'package:get/get.dart';
 import 'package:noisechecker/utils/styles.util.dart';
@@ -33,6 +34,39 @@ class _MainScreenState extends State<MainScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  Get.toNamed(WebScreen.routeName);
+                },
+                child: Container(
+                    width: 90.w,
+                    padding: EdgeInsets.all(5.w),
+                    decoration: BoxDecoration(
+                        color: ColorPalettes.white,
+                        borderRadius: BorderRadius.all(Radius.circular(25))),
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                          flex: 9,
+                          child: PrimaryText(
+                            text:
+                                "Check the regulation about the limit of exhaust noise on the official Korlantas Polri website",
+                            fontSize: 10.sp,
+                            color: ColorPalettes.black,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Icon(Icons.arrow_forward_ios,
+                              color: ColorPalettes.black, size: 16.sp),
+                        )
+                      ],
+                    )),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
               Text(
                 'MainScreen',
                 style: TextStyle(color: ColorPalettes.grayLight),
