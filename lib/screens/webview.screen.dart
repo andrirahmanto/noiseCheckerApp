939cd972d2a873608b3358a7c6308975c035e742
Andri Rahmanto <rahmanto660@gmail.com>
2021-12-16 15:55:15 +0700

    add web screen

--- lib/screens/webview.screen.dart
+++ lib/screens/webview.screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:noisechecker/utils/custom_text.util.dart';
+import 'package:noisechecker/utils/styles.util.dart';
+import 'package:sizer/sizer.dart';
 
 class WebScreen extends StatelessWidget {
   static const routeName = '/webscreen';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: PrimaryText(
+          text: "Exhaust dB Checker",
+          fontSize: 14.0.sp,
+          color: ColorPalettes.grayLight,
+        ),
+        centerTitle: true,
+        leading: BackButton(
+          onPressed: () {
+            Get.back();
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'MainScreen',
+                style: TextStyle(color: ColorPalettes.grayLight),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
