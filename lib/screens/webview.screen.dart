3fbf77ab5b3974a55a259b7168c25899cc635d5f
Andri Rahmanto <rahmanto660@gmail.com>
2021-12-16 21:52:37 +0700

    feat: add webview screen (done)

--- lib/screens/webview.screen.dart
+++ lib/screens/webview.screen.dart
@@ -1,18 +1,36 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:noisechecker/utils/custom_text.util.dart';
 import 'package:noisechecker/utils/styles.util.dart';
 import 'package:sizer/sizer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class WebScreen extends StatelessWidget {
+class WebScreen extends StatefulWidget {
   static const routeName = '/webscreen';
+  @override
+  _WebScreenState createState() => _WebScreenState();
+}
+
+class _WebScreenState extends State<WebScreen> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: PrimaryText(
-          text: "Exhaust dB Checker",
+          text: "Web Screen",
           fontSize: 14.0.sp,
           color: ColorPalettes.grayLight,
         ),
@@ -23,19 +41,50 @@ class WebScreen extends StatelessWidget {
           },
         ),
       ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'MainScreen',
-                style: TextStyle(color: ColorPalettes.grayLight),
-              ),
-            ],
-          ),
-        ),
+      body: WebView(
+        initialUrl:
+            "https://korlantas.polri.go.id/news/razia-knalpot-brong-berikut-ukuran-maksimal-kebisingan-yang-diperbolehkan/",
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
       ),
     );
   }
 }
+// class WebScreen extends StatelessWidget {
+//   static const routeName = '/webscreen';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: PrimaryText(
+//           text: "Web Screen",
+//           fontSize: 14.0.sp,
+//           color: ColorPalettes.grayLight,
+//         ),
+//         centerTitle: true,
+//         leading: BackButton(
+//           onPressed: () {
+//             Get.back();
+//           },
+//         ),
+//       ),
+//       body: SafeArea(
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               SingleChildScrollView(
+//                 child: WebView(
+//                   initialUrl: 'https://flutter.dev',
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
